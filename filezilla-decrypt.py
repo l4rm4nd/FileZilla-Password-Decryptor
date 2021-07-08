24cfa995a74365b369368dd5ba023666771a2a41
LRVT <laurent.vetter@protonmail.com>
2021-07-08 13:21:28 +0200

    Update filezilla-decrypt.py
    fix typo

--- filezilla-decrypt.py
+++ filezilla-decrypt.py
@@ -3,7 +3,7 @@ from time import sleep
 
 # test data which work for the password "toto"
 # Location: "C:\Program Files (x86)\FileZilla Server\FileZilla Server.xml"
-# Remember to unescapte your SALT string, since FileZilla will store &gt; for < etc. 
+# Remember to unescape your SALT string, since FileZilla will store &gt; for < etc. 
 password = '8112E67312B4EF84DB1A4F969C21E9405577162ACF761F26F1603C62BBA6046E90C7E13F696C1D2737F7196374DB673FB82E59D7C089C4F1134991698A08EC09'.lower()
 salt = '`!U3`CQ;a&3IzbXc/4Wpb\\)OZ3TsXP;\'Wx#^K"Tu_XX.K\'o<\'c&A:vItTX-M|Z0Y'.encode('utf-8')
 
